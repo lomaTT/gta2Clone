5f6657a293b5315f3b2b4944d15698f20ed0b893
alexey <alexeyttdeveloper@gmail.com>
2023-04-25 18:28:42 +0200

    kill enemy logic added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { Player } from "./player.js";
 import {Projectile} from "./projectile.js";
+import {Enemy} from "./enemy.js";
+import {distanceBetweenTwoPoints} from "./utilities.js";
 
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
@@ -8,12 +10,15 @@ canvas.height = document.documentElement.clientHeight;
 
 let player;
 let projectiles = [];
+let enemies = [];
+let particles = [];
 
 startGame();
 
 function startGame() {
     init();
     animate();
+    spawnEnemies();
 }
 
 function init() {
@@ -25,6 +30,46 @@ function init() {
     };
     player = new Player(canvas.width/2, canvas.height/2, context, movementLimits);
     addEventListener("click", createProjectile);
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    particles = particles.filter(particle => particle.alpha > 0);
+    projectiles = projectiles.filter(projectileInsideWindow);
+    enemies.forEach(enemy => checkHittingEnemy(enemy));
+    enemies = enemies.filter(enemy => enemy.health > 0);
+    // console.log(projectiles);
+
+    particles.forEach(particle => particle.update());
+    projectiles.forEach(projectile => projectile.update());
+    player.update();
+
+    enemies.forEach(enemy => enemy.update());
+}
+
+function spawnEnemies() {
+    enemies.push(new Enemy(canvas.width, canvas.height, context, player));
+}
+
+function checkHittingEnemy(enemy) {
+    projectiles.some((projectile, index) => {
+        const distance = distanceBetweenTwoPoints(projectile.x, projectile.y, enemy.x, enemy.y);
+        if (distance - enemy.radius - projectile.radius > 0) return false;
+
+        removeProjectileByIndex(index);
+        enemy.health--;
+        if (enemy.health < 1) {
+            enemy.createExplosion(particles);
+        }
+
+        return true;
+    })
+}
+
+function removeProjectileByIndex(index) {
+    projectiles.splice(index, 1);
 }
 
 function createProjectile(event) {
@@ -39,9 +84,7 @@ function createProjectile(event) {
     );
 }
 
-function animate() {
-    requestAnimationFrame(animate);
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    projectiles.forEach(projectile => projectile.update());
-    player.update();
+function projectileInsideWindow(projectile) {
+    return projectile.x + projectile.radius > 0 && projectile.x - projectile.radius < canvas.width &&
+        projectile.y + projectile.radius > 0 && projectile.y - projectile.radius < canvas.height;
 }
